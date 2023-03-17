8b15751f5cb54f8bf01cb13426035dbeec0e7537
Bystrova Ann <ann.bystrova96@mail.ru>
2023-03-17 04:00:29 +0300

    fix: fix build
    ref: #3

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,10 @@
+// import KeycloakMock from './keycloakMock';
+// import { customize } from './customize';
+// import { createPromise } from './utils';
+
 export { customize } from './customize';
-export type { ICustomize } from './types';
 export { createPromise } from './utils';
-export { default } from './keycloakMock';
+export { default as KeycloakMock } from './keycloakMock';
+
+export type { ICustomize } from './types';
+// export { KeycloakMock, customize, createPromise };
