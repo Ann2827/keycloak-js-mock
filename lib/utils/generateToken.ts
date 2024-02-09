3939642299a79278a48ab7332880cebe0a99588f
Bystrova Ann <ann.bystrova96@mail.ru>
2024-02-09 22:38:28 +0300

    feat: v19.0.3
    ref: #9

--- lib/utils/generateToken.ts
+++ lib/utils/generateToken.ts
@@ -1,14 +1,18 @@
-import { SignJWT, decodeJwt } from 'jose';
+import {SignJWT, decodeJwt, JWTPayload} from 'jose';
 import {IGenerateToken} from "../types";
 
-export const generateToken = ({ data, alg, expiration, secret }: IGenerateToken) =>
+export const generateToken = ({ data, alg, expiration, secret }: IGenerateToken): Promise<string> =>
   new SignJWT({ ...data })
     .setProtectedHeader({ alg })
     .setExpirationTime(expiration)
     .sign(new TextEncoder().encode(secret));
 
+export const decodeToken = (token: string): JWTPayload => {
+  return decodeJwt(token);
+};
+
 export const getExpByToken = (token: string): number => {
-  const exp = decodeJwt(token).exp;
+  const exp = decodeToken(token).exp;
   if (!exp) return 0;
   const time = exp - Math.floor(Date.now() / 1000);
   if (time <= 0) return 0;
