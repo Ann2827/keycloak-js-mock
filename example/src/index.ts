68f90298b6a053db934ce3fdb5acd9e079db626b
Bystrova Ann <ann.bystrova96@mail.ru>
2024-02-09 21:26:04 +0300

    feat: v17.0.0
    ref: #9

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,11 +1,20 @@
 import { customize, KeycloakMock } from 'keycloak-js-mock';
+import Keycloak from 'keycloak-js';
 import * as process from 'process';
 
 customize.profile({
   username: 'myUserName',
 });
 
-const keycloak = KeycloakMock();
+const useMocked = true;
+const keycloakInit = (): Keycloak.KeycloakInstance => {
+  if (useMocked) {
+    return KeycloakMock()
+  }
+  return Keycloak();
+}
+
+const keycloak = keycloakInit();
 keycloak.init({}).then(() => {
   console.log('token', keycloak.token);
   console.log('profile', keycloak.profile);
